5b163c67924213b650af3cd9df5e75c15cb78301
Awais430 <hamzaaliics@gmail.com>
2024-05-04 02:34:35 -0700

    adding change to index.js

--- Server/index.js
+++ Server/index.js
@@ -1,43 +1,57 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
-const TodoModel = require('./Models/Todo')
+const cors = require("cors");
+const TodoModel = require("./Models/Todo");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect(
+  "mongodb+srv://hamza:awais%40786@cluster0.87g7xmx.mongodb.net/mydb?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-app.get('/get', (req, res) => {
-    TodoModel.find()
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+const db = mongoose.connection;
 
-app.put('/update/:id', (req, res) => {
-    const {id} = req.params;
-    TodoModel.findByIdAndUpdate({_id: id}, {done: true})
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+db.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
 
-app.delete('/delete/:id', (req, res) => {
-    const {id} = req.params;
-    TodoModel.findByIdAndDelete({_id: id})
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+db.once("open", () => {
+  console.log("Connected to MongoDB successfully");
+});
 
-app.post('/add', (req, res) => {
-    const task = req.body.task;
-    TodoModel.create({
-        task: task
-    }).then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+app.get("/get", (req, res) => {
+  TodoModel.find()
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
+
+app.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  TodoModel.findByIdAndUpdate({ _id: id }, { done: true })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  TodoModel.findByIdAndDelete({ _id: id })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
+
+app.post("/add", (req, res) => {
+  const task = req.body.task;
+  TodoModel.create({
+    task: task,
+  })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
 
 app.listen(3001, () => {
-    console.log("Server is Running")
-})
+  console.log("Server is Running");
+});
